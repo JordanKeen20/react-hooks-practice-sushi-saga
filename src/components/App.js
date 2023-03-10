f9d362c910bc66692317a1b476d7a62172848ec1
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2023-03-10 14:31:44 -0800

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [aquireSushi, setAquireSushi] = useState ([])
+  const [getPlates, setGetPlates] = useState([])
+  const [walletAmount, setWalletAmount] = useState(100)
+
+  useEffect (()=>{
+    fetch(API)
+    .then (r =>r.json())
+    .then(data => setAquireSushi(data))
+    .catch (error => console.log ("issue with start up"))
+  }, [])
+
+  function handleGetPlates(price){
+    setGetPlates([...getPlates, parseInt(price)])
+    setWalletAmount(walletAmount - parseInt(price))
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis = {aquireSushi}
+       handlePlates = {handleGetPlates} 
+       wallet={walletAmount}/>
+      <Table 
+      plates={getPlates} wallet={walletAmount}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, handlePlates, wallet}) {
+  const [sushiInfo, setSushiInfo] = useState (1)
+
+  const start = sushiInfo
+  const end = (sushiInfo + 4)
+  
+  const beltDishes = sushis.filter((sushi) => {return sushi.id >= start && sushi.id < end})
+
+  function handleMore(){
+    if (end > 100){
+      return setSushiInfo(1)
+    } return setSushiInfo(end)
+  }
+
+  const renderSushi = beltDishes.map((dish) => {
+    return (
+      <Sushi
+          key={dish.id}
+          image={dish.img_url}
+          name={dish.name}
+          price={dish.price}
+          plateHandler={handlePlates}
+          wallet={wallet}
+      />
+    )
+  })
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushi}
+      <MoreButton moreSushi ={handleMore} />
     </div>
   );
 }
